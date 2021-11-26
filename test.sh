f9a51a7e2959834d7da2ea30850f96acf39ca8a7
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2021-11-26 16:08:34 +0100

    UPDATE : trigger dest workflow

---
+++ test.sh
@@ -0,0 +1,19 @@
+curl -XGET https://api.github.com/repos/$DEST_REPO/actions/workflows \
+  --header "'Authorization: Bearer $ACCESS_TOKEN'" \
+  --header 'Content-Type: application/json' \
+  > output.json
+
+# jq < './output.json' ".workflows[] | select(.name == \"$WORKFLOW_NAME\") | {url, html_url} | \"URL=\(.url) REF=\(.html_url)\""
+
+export $(cat output.json | jq -r --arg WORKFLOW_NAME "$WORKFLOW_NAME" \
+'select(.workflows | length > 0) | .workflows[] | select(.name == $WORKFLOW_NAME) | {url, html_url} | "URL=\(.url) REF=\(.html_url)" | sub("(?<head>REF=).*\/blob\/(?<ref>.*)\/\\.github.*";"\(.head)\(.ref)")')
+
+echo "---"
+echo "[LOG] URL & REF"
+echo $URL
+echo $REF
+
+curl --location --request POST $URL/dispatches \
+  --header "'Authorization: Bearer $ACCESS_TOKEN'" \
+  --header 'Content-Type: application/json' \
+  --data-raw "'{\"ref\": \"$REF\"}'"
